59b1a016efb2bd14f36a8e5a7b5d7e4c47fa60e6
angie <angelaclangat@gmai.com>
2025-06-17 09:27:20 +0300

    final commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask_migrate import Migrate
 from models import db, User
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://my_database_0upx_user:b08KI2ByRKUXQCchx9UkkxnViHO8k59B@dpg-d15vd8p5pdvs73e30b50-a.oregon-postgres.render.com/my_database_0upx'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://my_database_0upx_user:b08KI2ByRKUXQCchx9UkkxnViHO8k59B@dpg-d15vd8p5pdvs73e30b50-a.oregon-postgres.render.com/my_database_0upx/user_app_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
